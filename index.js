9ad1ab307d7b94641e544462c82ee64795b42ba4
Luis Nitro 5 <luis.ramos@juiceweb.mx>
2021-05-16 19:20:31 -0500

    a

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const users = require("./users");
 const recipes = require("./recipes");
-// const ingredientes = require("./ingredientes");
-// const categorias = require("./categorias");
+const ingredientes = require("./ingredientes");
+const categorias = require("./categorias");
 const Tag = require("./models/Tags");
 var mongoose = require("./conexion");
 const { log, test } = require("./middlewares/logs");
 var Consignador = require("./models/User");
 const Utensilio = require("./models/Utensilios");
-const ingredientes = require("./models/Ingredientes");
-const Categoria = require("./models/Categorias");
+// const ingredientes = require("./models/Ingredientes");
+// const Categoria = require("./models/Categorias");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
@@ -26,20 +26,20 @@ app.use("/api/Ingrediente", ingredientes);
 app.use("/api/Categoria", categorias);
 
 
-app.get("/api/ingredientes", async (req, res) => {
-  let lista = await Ingrediente.getIngredientes();
-  res.send(lista);
-});
+// app.get("/api/ingredientes", async (req, res) => {
+//   let lista = await Ingrediente.getIngredientes();
+//   res.send(lista);
+// });
 
 app.get("/api/Utensilio", async (req, res) => {
   let lista = await Utensilio.getUtensilio();
   res.send(lista);
 });
 
-app.get("/api/Categoria" , async (req, res) => {
-  let lista= await Categoria.getCategoria();
-  res.send(lista);
-});
+// app.get("/api/Categoria" , async (req, res) => {
+//   let lista= await Categoria.getCategoria();
+//   res.send(lista);
+// });
 
 app.get("/api/Tag" , async (req, res) => {
   let lista= await Tag.getTag();
